fbecec5cc3539c74be4ae8766157e91c2ed9998f
Donald Tran <tran.donald@gmail.com>
2017-03-29 13:27:16 -0700

    PHP Includes - Donald Tran

---
+++ php_includes/index_main.php
@@ -0,0 +1,3 @@
+<?php include('main.php'); ?>
+<div>I'm not in the main content or the footer</div>
+<?php include('footer.php'); ?>
---
+++ php_includes/main.php
@@ -0,0 +1,5 @@
+<main id="main_content">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus excepturi necessitatibus officiis sed?
+    Accusamus consequuntur, odit possimus sapiente voluptates voluptatum.
+    <br><a href="https://raw.githubusercontent.com/sindresorhus/meow/master/meow.gif">Meow</a>
+</main>
