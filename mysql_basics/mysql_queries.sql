28907b627cf19221cc6ef6f20dafd56275db771e
Donald Tran <tran.donald@gmail.com>
2017-04-01 17:37:51 -0700

    MySQL Basics - Donald Tran

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -1,1 +1,6 @@
 Place your queries below
+SELECT * FROM `todo_items` WHERE `user_id`=2;
+INSERT INTO `todo_items` SET `id`=null, `title`='nigiri', `details`='gimme dat eel sauce', `timestamp`=null, `user_id`=2;
+DELETE FROM `todo_items` WHERE `title`='nigiri';
+UPDATE `todo_items` SET `title`='karaage' WHERE `title`='bento';
+SELECT * FROM `users` WHERE `id`=2;
