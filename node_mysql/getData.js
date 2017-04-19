fbd6b9879aa8078406f972f2905558072a0219c0
Donald Tran <tran.donald@gmail.com>
2017-04-19 13:12:53 -0700

    Testing git add -i

--- node_mysql/getData.js
+++ node_mysql/getData.js
@@ -1,6 +1,16 @@
 //require your modules: express, and mysql
+var mysql = require('mysql');
+var express = require('express');
+var server = express();
 
 //define your connection info for mysql
+var conn = mysql.createConnection({
+    host:       'localhost',
+    port:       '8889',
+    user:       'root',
+    password:   'root',
+    database:   'c11.16_db'
+});
 
 //define your handler for your mysql connection callback
 	//perform your mysql query call
@@ -12,3 +22,51 @@
 //define your handler for the express web request
 
 //set up your express server and start listening
+server.put('/student', function (request, response) {
+   if (request.query.name === undefined || request.query.course === undefined || request.query.grade === undefined){
+       response.send(json.stringify({success: false, error:['must supply name, course, and grade']}))
+   }
+    conn.connect(function () {
+        conn.query(`INSERT INTO students name='${request.query.name}', course='${request.query.course}', grade='${request.query.grade}'`, function (error, data, fields) {
+            var output = {
+                success: true,
+                data: data
+            };
+            var output_json = json.stringify(output);
+            response.send(output_json);
+        })
+    })
+});
+server.get('/student',function(request, response){
+    if(request.query.id!==undefined) {
+        conn.connect(function () {
+            conn.query('SELECT * FROM students WHERE id = ' + request.query.id, function (error, data, fields) {
+                var output = {
+                    success: true,
+                    data: data
+                };
+                var output_json = json.stringify(output);
+                response.send(output_json);
+            })
+        })
+    }
+});
+server.get('/getAll',function(request, response){
+    if(request.query.id!==undefined) {
+        conn.connect(function () {
+            conn.query('SELECT * FROM students WHERE id = ' + request.query.id, function (error, data, fields) {
+                var output = {
+                    success: true,
+                    data: data
+                };
+                var output_json = json.stringify(output);
+                response.send(output_json);
+            })
+        })
+    }
+});
+
+server.listen(1337,function(){
+    console.log('server is up');
+
+});
